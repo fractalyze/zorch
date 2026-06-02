42587c5acb5a826a74adb6fe66242f542d152ffb
Ryan Kim <chokobole33@gmail.com>
2026-06-02 17:00:57 +0900

    test: add deterministic rand_field helper

---
+++ zorch/testkit/random_field_test.py
@@ -0,0 +1,24 @@
+# Copyright 2026 The Zorch Authors. SPDX-License-Identifier: Apache-2.0
+import jax.numpy as jnp
+import zk_dtypes
+from absl.testing import absltest
+
+from zorch.testkit.random_field import rand_field
+
+
+class RandFieldTest(absltest.TestCase):
+    def test_shape_dtype_deterministic(self):
+        a = rand_field(0, (8,), zk_dtypes.koalabear)
+        b = rand_field(0, (8,), zk_dtypes.koalabear)
+        self.assertEqual(a.shape, (8,))
+        self.assertEqual(a.dtype, zk_dtypes.koalabear)
+        self.assertTrue(bool(jnp.all(a == b)))  # same seed -> same values
+
+    def test_different_seed_differs(self):
+        a = rand_field(0, (16,), zk_dtypes.koalabear)
+        b = rand_field(1, (16,), zk_dtypes.koalabear)
+        self.assertFalse(bool(jnp.all(a == b)))
+
+
+if __name__ == "__main__":
+    absltest.main()
